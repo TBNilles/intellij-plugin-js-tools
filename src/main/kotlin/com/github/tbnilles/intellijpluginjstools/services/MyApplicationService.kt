00ed092d740296a6dc3a6237b6c88c20cc1f93d7
GitHub Action <action@github.com>
2021-07-31 09:32:35 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/tbnilles/intellijpluginjstools/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.tbnilles.intellijpluginjstools.services
+
+import com.github.tbnilles.intellijpluginjstools.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/tbnilles/intellijpluginjstools/services/MyProjectService.kt
+++ src/main/kotlin/com/github/tbnilles/intellijpluginjstools/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.tbnilles.intellijpluginjstools.services
 
+import com.github.tbnilles.intellijpluginjstools.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
